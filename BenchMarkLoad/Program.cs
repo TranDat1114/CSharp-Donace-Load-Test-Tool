dc6367be90b202c6e0206032cd1a550c19f93ee3
trandat1114 <cctalk1114@gmail.com>
2023-12-14 01:21:56 +0700

    Delete unused files and dependencies

--- BenchMarkLoad/Program.cs
+++ BenchMarkLoad/Program.cs
@@ -4,200 +4,202 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
 using AsciiChart.Sharp;
-namespace BenchMarkLoad;
-class Program
+
+namespace BenchMarkLoad
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        if (args.Length < 2)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Usage: BenchmarkTool <url> <numRequests> <jsonFolderPath>");
-            return;
-        }
-
-        string url = args[0];
-        if (!Uri.TryCreate(url, UriKind.Absolute, out _))
-        {
-            Console.WriteLine("Invalid URL");
-            return;
-        }
-
-        if (!int.TryParse(args[1], out int numRequests) || numRequests <= 0)
-        {
-            Console.WriteLine("Invalid number of requests");
-            return;
-        }
-
-        string? jsonFolderPath = args.Length > 2 ? args[2] : null;
-
-        Console.WriteLine($"Benchmarking {url} with {numRequests} requests...");
-
-        await RunBenchmark(url, numRequests, jsonFolderPath);
-    }
-
-
-
-    static async Task RunBenchmark(string url, int numRequests, string? jsonFolderPath)
-    {
-        using HttpClient client = new();
-        Stopwatch stopwatch = new();
-        float averageResponseTime = 0;
-        float averageResponseTimePerShowResult = 0;
-        int successfulRequests = 0;
-        int failedRequests = 0;
-        int numRequestsPerXTime;
-        int numShowResultPerXTime = numRequests / (numRequests < 100 ? numRequests : numRequests / 100);
-
-        if (numRequests < 100)
-        {
-            numRequestsPerXTime = numRequests;
-        }
-        else
-        {
-            numRequestsPerXTime = numRequests / 100;
-        }
-
-        float averageResponseTimeAfterXRequest = 0;
-
-        string[] failedRequestsLog = [];
-
-        double[][] series = [new double[100], new double[100]];
-
-        int seriesPosition = 0;
-
-        string[] jsonFiles = jsonFolderPath != null ? Directory.GetFiles(jsonFolderPath, "*.json") : [];
-
-        if (jsonFiles.Length == 0 && jsonFolderPath != null)
-        {
-            Console.WriteLine("No JSON files found in the specified folder.");
-            return;
-        }
-
-        for (int i = 0; i < numRequests; i++)
-        {
-
-            if (i % numShowResultPerXTime == 0)
+            Console.OutputEncoding = Encoding.UTF8;
+            int numRequests;
+            string url;
+            string? jsonFolderPath;
+            if (args.Length == 0)
             {
-                Console.WriteLine($"After {i} Request| Successful: {successfulRequests}, Failed: {failedRequests}, Average response time: {averageResponseTimePerShowResult / numShowResultPerXTime} ms");
-                averageResponseTimePerShowResult = 0;
+                numRequests = 100000;
+                jsonFolderPath = @"D:\Code\DoAnTotNghiep\tool\LoadTest\User";
+                url = @"http://171.245.205.120:8082/api/Authentication/register";
             }
-            stopwatch.Restart();
-            HttpResponseMessage response;
-            if (jsonFolderPath != null && i < jsonFiles.Length)
+            else
             {
-                response = await SendRequestWithJson(client, url, jsonFiles[i]);
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Usage: BenchmarkTool <url> <numRequests> <jsonFolderPath>");
+                    return;
+                }
+
+                url = args[0];
+                if (!Uri.TryCreate(url, UriKind.Absolute, out _))
+                {
+                    Console.WriteLine("Invalid URL");
+                    return;
+                }
+
+                if (!int.TryParse(args[1], out numRequests) || numRequests <= 0)
+                {
+                    Console.WriteLine("Invalid number of requests");
+                    return;
+                }
+
+                jsonFolderPath = args.Length > 2 ? args[2] : null;
+            }
+
+
+            Console.WriteLine($"Benchmarking {url} with {numRequests} requests...");
+
+            await RunBenchmark(url, numRequests, jsonFolderPath);
+
+            Environment.Exit(0);
+        }
+
+        static async Task<HttpResponseMessage> SendRequestWithJson(HttpClient client, string url, string jsonFileName)
+        {
+            var jsonContent = string.Empty;
+
+            if (!string.IsNullOrEmpty(jsonFileName) && File.Exists(jsonFileName))
+            {
+                jsonContent = await File.ReadAllTextAsync(jsonFileName);
+            }
+
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            return await client.PostAsync(url, content);
+        }
+
+        static async Task RunBenchmark(string url, int numRequests, string? jsonFolderPath)
+        {
+            using HttpClient client = new();
+            client.Timeout = TimeSpan.FromSeconds(200);
+            float averageResponseTime = 0;
+
+            int successfulRequests = 0;
+            int failedRequests = 0;
+
+            List<string> failedRequestsLog = [];
+
+            double[][] series = [new double[100], new double[100]];
+
+            int numRequestsPerXTime = numRequests < 100 ? 1 : numRequests / 100;
+
+            int seriesIndex = 0;
+
+            string[] jsonFiles = jsonFolderPath != null ? Directory.GetFiles(jsonFolderPath, "*.json") : [];
+
+            if (jsonFiles.Length == 0 && jsonFolderPath != null)
+            {
+                Console.WriteLine("No JSON files found in the specified folder.");
+                return;
+            }
+
+            var tasks = new List<Task<ReponseTime>>();
+
+            for (int i = 0; i < numRequests; i++)
+            {
+                tasks.Add(SendRequestAsync(client, url, jsonFolderPath, jsonFiles, i));
+
+                if (tasks.Count == Environment.ProcessorCount || i == numRequests - 1)
+                {
+                    await Task.WhenAll(tasks);
+                    foreach (var task in tasks)
+                    {
+                        ReponseTime responseTime = await task;
+
+                        if (responseTime.Response!.IsSuccessStatusCode)
+                        {
+                            if (responseTime.Index % numRequestsPerXTime == 0)
+                            {
+                                Console.WriteLine($@"Successful: {successfulRequests}, Failed: {failedRequests}, Average response time: {averageResponseTime / successfulRequests + failedRequests} ms");
+                                series[0][seriesIndex] = responseTime.Time;
+                                seriesIndex++;
+                            }
+                            successfulRequests++;
+                        }
+                        else
+                        {
+                            var statusCode = responseTime.Response.StatusCode.ToString();
+                            if (!failedRequestsLog.Contains(statusCode))
+                            {
+                                failedRequestsLog.Add(statusCode);
+                            }
+                            if (responseTime.Index % numRequestsPerXTime == 0)
+                            {
+                                Console.WriteLine($@"Successful: {successfulRequests}, Failed: {failedRequests}, Average response time: {averageResponseTime / successfulRequests + failedRequests} ms");
+                                series[1][seriesIndex] = responseTime.Time;
+                                seriesIndex++;
+
+                            }
+                            failedRequests++;
+                        }
+                        averageResponseTime += responseTime.Time;
+                    }
+
+                    tasks.Clear();
+                }
+            }
+
+            Console.WriteLine($"\nSummary:");
+            Console.WriteLine($"Total Requests: {numRequests}");
+            Console.WriteLine($"Successful Requests: {successfulRequests}");
+            Console.WriteLine($"Failed Requests: {failedRequests}");
+            Console.WriteLine($"Average response time: {averageResponseTime / numRequests} ms");
+
+            if (failedRequestsLog.Count != 0)
+            {
+                Console.WriteLine($"Failed requests log:");
+            }
+            foreach (var item in failedRequestsLog)
+            {
+                Console.WriteLine($"Failed request reason: {item}");
+            }
+
+            Console.WriteLine($"Chart: Reponse time per {numRequestsPerXTime} requests");
+            Console.WriteLine(AsciiChart.Sharp.AsciiChart.Plot(
+                    series,
+                    new Options
+                    {
+                        AxisLabelFormat = "000.000 ms",
+                        Fill = '·',
+                        Height = 15,
+                        SeriesColors =
+                        [
+                            AnsiColor.Green,
+                            AnsiColor.Red,
+                        ],
+                        AxisLabelLeftMargin = 1,
+                        LabelColor = AnsiColor.Aqua,
+                    }));
+        }
+
+        static async Task<ReponseTime> SendRequestAsync(HttpClient client, string url, string? jsonFolderPath, string[] jsonFiles, int index)
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            HttpResponseMessage response;
+
+            if (jsonFolderPath != null && index < jsonFiles.Length)
+            {
+                response = await SendRequestWithJson(client, url, jsonFiles[index]);
             }
             else
             {
                 response = await client.GetAsync(url);
             }
+
             stopwatch.Stop();
-
-            if (numRequests < 100)
+            return new()
             {
-                if (response.IsSuccessStatusCode)
-                {
-
-                    series[0][seriesPosition] = stopwatch.ElapsedMilliseconds;
-                    successfulRequests++;
-                    seriesPosition++;
-
-                }
-                else
-                {
-                    var statusCode = response.StatusCode.ToString();
-                    if (!failedRequestsLog.Contains(statusCode))
-                    {
-                        failedRequestsLog = [.. failedRequestsLog, statusCode];
-                    }
-                    series[1][seriesPosition] = stopwatch.ElapsedMilliseconds;
-                    failedRequests++;
-                    seriesPosition++;
-
-                }
-            }
-            else
-            {
-                if (response.IsSuccessStatusCode)
-                {
-                    if (i % numRequestsPerXTime == 0)
-                    {
-                        series[0][seriesPosition] = averageResponseTimeAfterXRequest / numRequestsPerXTime;
-                        averageResponseTimeAfterXRequest = 0;
-                        seriesPosition++;
-                    }
-                    successfulRequests++;
-                }
-                else
-                {
-                    var statusCode = response.StatusCode.ToString();
-                    if (!failedRequestsLog.Contains(statusCode))
-                    {
-                        failedRequestsLog = [.. failedRequestsLog, statusCode];
-                    }
-                    if (i % numRequestsPerXTime == 0)
-                    {
-                        series[1][seriesPosition] = averageResponseTimeAfterXRequest / numRequestsPerXTime;
-                        averageResponseTimeAfterXRequest = 0;
-                        seriesPosition++;
-                    }
-                    failedRequests++;
-                }
-            }
-
-
-            averageResponseTimeAfterXRequest += stopwatch.ElapsedMilliseconds;
-            averageResponseTimePerShowResult += stopwatch.ElapsedMilliseconds;
-            averageResponseTime += stopwatch.ElapsedMilliseconds;
+                Response = response,
+                Time = stopwatch.ElapsedMilliseconds,
+                Index = index,
+            };
         }
-
-        Console.WriteLine($"\nSummary:");
-        Console.WriteLine($"Total Requests: {numRequests}");
-        Console.WriteLine($"Successful Requests: {successfulRequests}");
-        Console.WriteLine($"Failed Requests: {failedRequests}");
-        Console.WriteLine($"Average response time: {averageResponseTime / numRequests} ms");
-
-        if (failedRequestsLog.Length != 0)
-        {
-            Console.WriteLine($"Failed requests log:");
-        }
-        foreach (var item in failedRequestsLog)
-        {
-            Console.WriteLine($"Failed request reason: {item}");
-        }
-
-        Console.WriteLine($"Chart: Reponse time per {numRequestsPerXTime} requests");
-        Console.WriteLine(AsciiChart.Sharp.AsciiChart.Plot(
-                series,
-                new Options
-                {
-                    AxisLabelFormat = "000.0 ms",
-                    Fill = '·',
-                    Height = 15,
-                    SeriesColors =
-                    [
-                        AnsiColor.Green,
-                        AnsiColor.Red,
-                    ],
-                    AxisLabelLeftMargin = 1,
-                    LabelColor = AnsiColor.Aqua,
-                }));
     }
-
-    static async Task<HttpResponseMessage> SendRequestWithJson(HttpClient client, string url, string jsonFileName)
+    public class ReponseTime
     {
-        // Tạo một đối tượng StringContent để chứa dữ liệu JSON
-        var jsonContent = string.Empty;
-
-        // Nếu có tên file JSON, đọc nội dung của file
-        if (!string.IsNullOrEmpty(jsonFileName) && File.Exists(jsonFileName))
-        {
-            jsonContent = File.ReadAllText(jsonFileName);
-        }
-
-        // Gửi request POST với dữ liệu JSON
-        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-        return await client.PostAsync(url, content);
+        public HttpResponseMessage? Response { get; set; }
+        public float Time { get; set; }
+        public int Index { get; set; }
     }
 }
